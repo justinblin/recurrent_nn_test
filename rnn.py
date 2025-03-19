0a9bb83b4bd1416b791e409dbddfc7b60baf4444
Justin Lin <justinblin@gmail.com>
2025-03-19 17:54:03 -0500

    add unicode to ascii and data

--- rnn.py
+++ rnn.py
@@ -1,4 +1,6 @@
 import torch
+import string
+import unicodedata
 
 # use GPU if possible
 device = torch.device('cpu')
@@ -6,4 +8,11 @@ if torch.cuda.is_available():
     decide = torch.device('cuda')
 torch.set_default_device(device)
 
-print(device)
+
+# process names from unicode into ascii
+allowed_char = string.ascii_letters + ' .,;\'_'
+def unicode_to_ascii(unicode_string):
+    return ''.join(curr_char for curr_char in unicodedata.normalize('NFD', unicode_string) 
+                   if unicodedata.category != 'Mn' and curr_char in allowed_char)
+
+print(unicode_to_ascii('Ślusàrski'))
